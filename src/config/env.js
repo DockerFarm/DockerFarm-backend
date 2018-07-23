56a0d3f6de9766eba390a83b98af2fa898fe9794
y0c <wonder789@naver.com>
2018-07-24 02:24:08 +0900

    feat(passport): github config added

--- src/config/env.js
+++ src/config/env.js
@@ -3,5 +3,11 @@ export default (env) => ({
     jwtSecret: env.JWT_SECRET,
     sha256Secret : env.SHA256_SECRET,
     mongoUrl : env.MONGO_URL,
-    port: env.PORT
+    port: env.PORT,
+    github : {
+        clientId : env.GITHUB_CLIENT_ID,
+        clientSecret: env.GITHUB_CLIENT_SECRET,
+        callbackUrl : env.GITHUB_CALLBACK_URL,
+        scope: env.GITHUB_SCOPE
+    }
 });
