914bc5925d92c4d0ec663bf0542bf11f40cf9656
HoSung Lim <holnet1026@gmail.com>
2018-07-24 02:25:37 +0900

    feat(passport): auth github route added

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -1,10 +1,13 @@
 import Router from 'koa-router';
 import * as authCtrl from './auth.ctrl';
+import passport from 'config/passport';
 
 const router = new Router();
 
 
 router.post('/register', authCtrl.register);
 router.post('/login', authCtrl.login);
+router.get('/github', passport.authenticate('github'));
+router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), ctx => ctx.redirect('/'));
 
 export default router;
