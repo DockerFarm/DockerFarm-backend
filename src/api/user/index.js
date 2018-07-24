45f4f4fa9b3fc5f44922185f92e1b376f6fb6848
HoSung Lim <holnet1026@gmail.com>
2018-07-24 13:04:50 +0900

    feat(middleware): change auth middleware to jwtAuth

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -1,8 +1,13 @@
 import Router from 'koa-router';
 import * as userCtrl from './user.ctrl';
+import authMiddleware from 'lib/middleware/authMiddleware';
+import {
+    passport,
+    jwtAuth
+} from 'config/passport';
 
 const router = new Router();
 
-router.get('/me', userCtrl.me);
+router.get('/me', jwtAuth() ,userCtrl.me);
 
 export default router;
