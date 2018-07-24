c3e6bbfff5538f0105a16f5e5389dfde2e210010
HoSung Lim <holnet1026@gmail.com>
2018-07-24 13:03:17 +0900

    feat(passport): google strategy added

--- src/config/passport/index.js
+++ src/config/passport/index.js
@@ -1,5 +1,6 @@
 import passport from 'koa-passport';
 import JWTStrategy from './jwt';
+import GoogleStrategy from './google';
 import GithubStrategy from './github';
 
 
@@ -13,5 +14,15 @@ passport.serializeUser((user, done) => {
 
  passport.use(JWTStrategy);
  passport.use(GithubStrategy);
+ passport.use(GoogleStrategy);
 
- export default passport;
+ const jwtAuth = () => passport.authenticate('jwt', { session: false});
+ const githubAuth = opts => passport.authenticate('github', opts);
+ const googleAuth = opts => passport.authenticate('google', opts);
+
+ export {
+     jwtAuth,
+     githubAuth,
+     googleAuth,
+     passport
+ };
