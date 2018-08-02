ac292acb0bb462554b64b948fe8481479fcb3d1d
HoSung Lim <holnet1026@gmail.com>
2018-08-02 19:09:51 +0900

    feat(user): extract endpoint schema

--- src/db/models/User.js
+++ src/db/models/User.js
@@ -1,19 +1,14 @@
 import mongoose from 'mongoose';
 import crypto from 'crypto';
 import config from 'config';
+import EndPoint, { EndPointSchema } from './EndPoint';
 const { Schema } = mongoose;
 
 const User = new Schema({
     email: String,
     password: String,
     username: String,
-    endpoints: [
-        {
-            url: { type : String },
-            name : { type: String },
-            isActive : { type : Boolean }
-        }    
-    ],
+    endpoints: [ EndPointSchema ],
     createdAt: {
         type: Date,
         default : Date.now
@@ -36,6 +31,63 @@ const User = new Schema({
 
 const encryptPassword = password => {
     return crypto.createHmac('sha256', config.sha256Secret).update(password).digest('hex');
+};
+
+User.statics.updateEndpoint = async function({
+    userId,
+    endpointId,
+    name,
+    url,
+    tls
+}) {
+    const user = await this.findOne({_id: userId}).exec();
+
+    let endpoint = user.endpoints.id(endpointId);
+
+    endpoint.name = name;
+    endpoint.url = url;
+    endpoint.tls = tls;
+    
+    return user.save();
+}
+User.statics.removeEndpoint = async function({
+    userId,
+    endpointId
+}) {
+   const user = await this.findOne({_id: userId}).exec();
+   user.endpoints.id(endpointId).remove();
+   return user.save();
+};
+
+User.statics.selectEndpoint = async function({
+    url,
+    name
+}) {
+    return this.find().or([{ 'endpoints.url': url}, {'endpoints.name': name }]).exec();
+};
+
+User.statics.selectAllEndpoints = async function({
+    _id
+}) {
+    return this.findOne({_id}).select('endpoints').exec();
+};
+
+User.statics.addEndpoint = async function({
+    _id,
+    name,
+    url,
+    tls
+}) {
+    let endpoint = new EndPoint({
+        name,
+        url,
+        tls
+    });
+    
+    const user = await this.findOne({ _id }).exec();
+    user.endpoints.push(endpoint);
+
+    return user.save();
 };
 
 User.statics.localSignup = function({
