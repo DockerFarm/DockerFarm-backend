fe3d92f3d7065ca60f8670483c2c22850a2dfe4a
HoSung Lim <holnet1026@gmail.com>
2018-08-05 23:11:56 +0900

    refactor(endpoint): extract endpoint from user embedded document

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -5,8 +5,4 @@ const router = new Router();
 
 router.get('/me',userCtrl.me);
 
-router.post('/endpoint', userCtrl.addEndpoint);
-router.get('/endpoint', userCtrl.selectAllEndpoints);
-router.delete('/endpoint/:id', userCtrl.removeEndpoint);
-
 export default router;
--- src/db/models/EndPoint.js
+++ src/db/models/EndPoint.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
+const ObjectId = mongoose.Types.ObjectId;
 const EndPoint = new Schema({
-    url: { type : String },
-    name : { type: String },
+    url: { type : String, unique: true },
+    name : { type: String, unique: true },
     tls: { type: Boolean, default: false },
     isActive : { type : Boolean, default: false },
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     createdAt: {
         type: Date,
         default : Date.now
@@ -16,5 +18,56 @@ const EndPoint = new Schema({
     }
 });
 
-export const EndPointSchema = EndPoint;
+EndPoint.statics.addEndpoint = function({
+    url,
+    tls,
+    name,
+    isActive,
+    userId
+}) {
+    const endpoint = new this({
+        url,
+        tls,
+        name,
+        isActive,
+        userId
+    });
+
+    return endpoint.save();
+}
+
+EndPoint.statics.selectEndpointsByUserId = function({
+    userId
+}) {
+    return this.find({ "userId": new ObjectId(userId) }).exec();
+}
+
+EndPoint.statics.removeEndpoint = function({
+    _id
+}) {
+    return this.deleteOne({_id}).exec();
+}
+
+EndPoint.statics.updateEndpoint = function({
+    _id,
+    tls,
+    name,
+    isActive,
+    url
+}) {
+    return this.update(
+        {_id}, 
+        { $set: {
+            tls,
+            name,
+            isActive,
+            url
+        }
+    }).exec();
+}
+
+EndPoint.statics.unActiveAll = function() {
+    return this.update({}, {isActive: false}, {multi: true}).exec();
+}
+
 export default mongoose.model('EndPoint', EndPoint);
