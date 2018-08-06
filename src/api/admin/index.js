e709fc862fcea7a703ce5d50228d181f17ac6f04
y0c <wonder789@naver.com>
2018-08-06 23:26:49 +0900

    feat(container): implement container docker remote api ✨

--- src/api/admin/index.js
+++ src/api/admin/index.js
@@ -1,9 +1,11 @@
 import Router from 'koa-router';
 import endpoint from './endpoint';
+import container from './container';
 
 const router = new Router;
 
 
 router.use('/endpoint', endpoint.routes());
+router.use('/container', container.routes());
 
 export default router;
