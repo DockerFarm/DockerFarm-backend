e72931f019e2d5ccf9f4ba9c580cf11e548682f6
HoSung Lim <holnet1026@gmail.com>
2018-08-08 23:40:36 +0900

    feat(container): implement command feature    start, stop, pause ...

--- src/api/admin/container/container.ctrl.js
+++ src/api/admin/container/container.ctrl.js
@@ -1,4 +1,5 @@
 import * as ContainerApi from 'lib/dockerApi/container';
+import Joi from 'joi';
 
 export const getContainerList = async ctx => {
     const { endpoint: { url } } = ctx.state.user;
@@ -39,25 +40,48 @@ export const getContainerInspectRaw = async ctx => {
 }
 
 
-export const startContainer = async ctx => {
+export const commandToContainer = async ctx => {
     const { endpoint: {url} } = ctx.state.user;
-    const { id } = ctx.params;
+    const { id, command } = ctx.params;
+    const { form } = ctx.request.body;
     
-    try {
-        const { data } = await ContainerApi.startContainer({url, id});
-        ctx.status = 200;
-        ctx.body = { result: data};
-    } catch(e) {
-        ctx.throw(e, 500);
-    }
-}
+    const commandMap = {
+        'start': ContainerApi.startContainer,
+        'restart': ContainerApi.restartContainer,
+        'stop' : ContainerApi.stopContainer,
+        'pause': ContainerApi.pauseContainer,
+        'resume': ContainerApi.resumeContainer,
+        'kill': ContainerApi.killContainer,
+        'remove': ContainerApi.removeContainer,
+        'update': ContainerApi.updateContainer
+    };
 
-export const stopContainer = async ctx => {
-    const { endpoint: {url} } = ctx.state.user;
-    const { id } = ctx.params;
-    
     try {
-        const { data } = await ContainerApi.stopContainer({url, id});
+        const schema = Joi.object().keys({
+            command: Joi.string().valid(
+                'start',
+                'restart',
+                'stop',
+                'pause',
+                'resume',
+                'kill',
+                'remove',
+                'update' 
+            ).required(),
+        });
+
+        let validateResult = schema.validate({ command });
+
+        if( validateResult.error != null ) {
+            ctx.status = 422;
+            ctx.body = { 
+                type : "ValidateError",
+                message : `invalid command ${command}`
+            };
+            return;
+        };
+
+        const { data } = await commandMap[command]({url, id, form});
         ctx.status = 200;
         ctx.body = { result: data};
     } catch(e) {
