81823b7ab742926e7c7b95f8fd0dfedd63f281c1
KuSangMo <rntkdah2005@gmail.com>
2018-08-13 21:38:38 +0900

    feat(network): implement network api except add network

--- src/api/admin/index.js
+++ src/api/admin/index.js
@@ -2,12 +2,13 @@ import Router from 'koa-router';
 import endpoint from './endpoint';
 import container from './container';
 import image from './image';
+import network from './network';
 
 const router = new Router;
-
 
 router.use('/endpoint', endpoint.routes());
 router.use('/container', container.routes());
 router.use('/image', image.routes());
+router.use('/network', network.routes());
 
 export default router;
---
+++ src/lib/dockerApi/network.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import { get, keys} from 'lodash';
+
+export const getNetworkList = (url) =>
+    axios.get(`${url}/networks`)
+        .then(resp => {
+          const transformObject = v => {
+              return {
+                name: get(v,'Name',''),
+                scope: get(v,'Scope','-'),
+                driver: get(v,'Driver','-'),
+                ipamdriver: get(v, 'IPAM.Driver','-'),
+                subnet: get(v, 'IPAM.Config[0].Subnet','-'),
+                gateway: get(v, 'IPAM.Config[0].Gateway','-'),
+              }
+          };
+          return resp.data.map(transformObject);
+        });
+
+export const getNetworkInfo = (url, id) =>
+    axios.get(`${url}/networks/${id}`)
+        .then(resp => {
+            const { data } = resp;
+            const id = keys(get(data, 'Containers', ''));
+
+
+              return {
+                network : {
+                    name: get(data,'Name',''),
+                    id: get(data,'Id', ''),
+                    driver: get(data,'Driver', ''),
+                    scope: get(data,'Scope', ''),
+                    subnet: get(data, 'IPAM.Config[0].Subnet','-'),
+                    gateway: get(data, 'IPAM.Config[0].Gateway','-'),
+                },
+                container: {
+                    name: get(data, `Containers.${id}.Name`, ''),
+                    ipv4: get(data, `Containers.${id}.IPv4Address`, '-'),
+                    ipv6: get(data, `Containers.${id}.IPv6Address`, '-'),
+                    mac: get(data, `Containers.${id}.MacAddress`, '-'),
+                }
+              }
+      });
+
+export const getNetworkInspectRaw = ({url, id}) => axios.get(`${url}/networks/${id}`);
+export const deleteNetwork = ({url, id}) => axios.delete(`${url}/networks/${id}`);
+//need to fix addNetwork
+export const addNetwork = (url) => axios.post(`${url}/networks/create`);
