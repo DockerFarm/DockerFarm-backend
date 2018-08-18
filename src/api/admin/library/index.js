27fde66855e760866c228606157a8020ec540846
KuSangMo <rntkdah2005@gmail.com>
2018-08-18 23:34:58 +0900

    refactor(library) : refactor library api

--- src/api/admin/library/index.js
+++ src/api/admin/library/index.js
@@ -3,6 +3,6 @@ import * as libraryCtrl from './library.ctrl';
 
 const router = new Router();
 
-router.get('/:page', libraryCtrl.getLibraryList);
+router.get('/', libraryCtrl.getLibraryList);
 
 export default router;
--- src/api/admin/library/library.ctrl.js
+++ src/api/admin/library/library.ctrl.js
@@ -1,9 +1,9 @@
 import { getRepoList } from 'lib/dockerHub'
 
 export const getLibraryList = async ctx => {
-  const { page } = ctx.params;
+  const { page, page_size } = ctx.request.query;
   try {
-      const data = await getRepoList(page);
+      const data = await getRepoList(page, page_size);
       ctx.status = 200;
       ctx.body = { result: data };
   } catch(e) {
