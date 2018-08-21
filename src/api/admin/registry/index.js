193c09c9b24f2c1988e32976c5ce9b64eda3a493
Hosung Lim <holnet1026@gmail.com>
2018-08-21 23:19:55 +0900

    refactor(registry): selectRegistry change to checkRegistry

--- src/api/admin/registry/index.js
+++ src/api/admin/registry/index.js
@@ -4,6 +4,7 @@ import * as registryCtrl from './registry.ctrl';
 const router = new Router();
 
 router.get('/', registryCtrl.selectRegistries);
+router.get('/:id', registryCtrl.selectRegistry);
 router.post('/', registryCtrl.addRegistry);
 router.put('/:id', registryCtrl.updateRegistry);
 router.delete('/:id', registryCtrl.removeRegistry);
