d09385d2597e36ea6a6a7d21a3f1087321eed1ee
Hosung Lim <holnet1026@gmail.com>
2018-08-23 23:19:29 +0900

    refactor(image): extract func to image ctrl

--- src/api/admin/image/image.ctrl.js
+++ src/api/admin/image/image.ctrl.js
@@ -1,27 +1,32 @@
 import * as ImageApi from 'lib/dockerApi/image';
+import request from 'request';
+import mkdirp from 'mkdirp';
+import { objectToQueryString } from 'lib/utility';
+import fs from 'fs-extra';
+import tar from 'tar';
 
 const extractNameAndTag = (imageName, registry) => {
   /* imageName.image = post body value */
-  const imageNameAndTag = imageName.image.split(':');
-  let image = imageNameAndTag[0];
-  const tag = imageNameAndTag[1] ? imageNameAndTag[1] : 'latest';
-  if (registry) {
-      image = registry + '/' + imageNameAndTag[0];
+    const imageNameAndTag = imageName.image.split(':');
+    let image = imageNameAndTag[0];
+    const tag = imageNameAndTag[1] ? imageNameAndTag[1] : 'latest';
+    if (registry) {
+        image = registry + '/' + imageNameAndTag[0];
     }
 
     return {
-      image: image,
-      tag: tag
+        image: image,
+        tag: tag
     };
 }
 
 const extractRepoAndTag = (tagName) => {
-  const imageRepoAndTag = extractNameAndTag(tagName);
+    const imageRepoAndTag = extractNameAndTag(tagName);
 
-  return {
-    repo: imageRepoAndTag.image,
-    tag: imageRepoAndTag.tag
-  };
+    return {
+        repo: imageRepoAndTag.image,
+        tag: imageRepoAndTag.tag
+    };
 }
 
 export const getImageList = async ctx => {
@@ -62,42 +67,127 @@ export const getImageInspectRaw = async ctx => {
     }
 }
 
-export const pullImage = async ctx => {
-  const { endpoint: {url} } = ctx.state.user;
-  const { image, tag } = extractNameAndTag(ctx.request.body);
-
-  try {
-      const { data } = await ImageApi.pullImage({url, image, tag});
-      ctx.status = 200;
-      ctx.body = { result: data };
-  } catch(e) {
-      ctx.throw(e, 500);
-  }
-}
-
 export const tagImage = async ctx => {
-  const { endpoint: {url} } = ctx.state.user;
-  const { id } = ctx.params;
-  const { repo, tag } = extractRepoAndTag(ctx.request.body);
+    const { endpoint: {url} } = ctx.state.user;
+    const { id } = ctx.params;
+    const { repo, tag } = extractRepoAndTag(ctx.request.body);
 
-  try {
-    const { data } = await ImageApi.tagImage({url, id, repo, tag});
-    ctx.status = 200;
-    ctx.body = { result: data };
-  } catch(e) {
-    ctx.throw(e, 500);
-  }
+    try {
+        const { data } = await ImageApi.tagImage({url, id, repo, tag});
+        ctx.status = 200;
+        ctx.body = { result: data };
+    } catch(e) {
+        ctx.throw(e, 500);
+    }
 }
 
 export const deleteImage = async ctx => {
-  const { endpoint: {url} } = ctx.state.user;
-  const { id } = ctx.params;
+    const { endpoint: {url} } = ctx.state.user;
+    const { id } = ctx.params;
 
-  try {
-      const { data } = await ImageApi.deleteImage({url, id});
-      ctx.status = 200;
-      ctx.body = { result: data };
-  } catch(e) {
-      ctx.throw(e, 500);
-  }
+    try {
+        const { data } = await ImageApi.deleteImage({url, id});
+        ctx.status = 200;
+        ctx.body = { result: data };
+    } catch(e) {
+        ctx.throw(e, 500);
+    }
 }
+
+
+export const pullImage = socket => 
+    (resp, fn) => {
+        const { endpoint: { url }} = socket.user;
+        const queryString = objectToQueryString(JSON.parse(resp)); 
+
+        let req = request({
+            method: 'POST',
+            uri: `${url}/images/create?${queryString}`
+        });
+
+        req.on( 'data', data => {
+            socket.emit('progress', data.toString());
+        });
+
+        req.on('end', _ => {
+            socket.emit('done');
+        })
+    };
+
+export const buildImage = socket => 
+     async (resp, fn) => {
+        const { endpoint: { url }} = socket.user;
+
+        let params = JSON.parse(resp);
+        const timestamp = +new Date();
+        const path = `/tmp/dockerfarm/${timestamp}`;
+
+        const removeTempFolder = _ => new Promise((resolve, reject) => {
+            fs.remove('/tmp/dockerfarm', err => {
+                resolve();
+            });
+        });
+
+        const makeTempFolder = _ => new Promise((resolve, reject) => {
+            mkdirp(path, err => {
+                resolve();
+            })
+        });
+
+        const writeDockerFile = _ => new Promise((resolve, reject) => {
+            fs.writeFile(`${path}/Dockerfile`, params.dockerfile, _ => {
+                resolve();
+            })
+        });
+
+        const requestBuild = _ => new Promise((resolve, reject) => {
+            let tags = '';
+
+            if ( params.names ) {
+                tags = params.names.map( v => (
+                    `t=${v}`
+                )).join('&');
+            }
+
+            let req = request({ method: 'POST',
+                uri:`${url}/build?${tags}`,
+                body: fs.createReadStream(`${path}/Dockerfile.tar.gz`)
+            })
+
+            try {
+                req.on('error', _ => {
+                    throw 'Error';
+                });
+                
+                req.on('data', function(data) {
+                    socket.emit('progress', data.toString());
+                })
+
+                req.on('end', _ => {
+                    socket.emit('done');
+                    resolve();
+                })
+
+            } catch(e) {
+                socket.emit('done');
+                resolve();
+            }
+
+        });
+        
+
+        try {
+            await removeTempFolder();
+            await makeTempFolder()
+            await writeDockerFile();
+            await tar.c({
+                gzip:true,
+                cwd: `${path}`,
+                file:`${path}/Dockerfile.tar.gz`,
+            },[`Dockerfile`]);
+            await requestBuild();
+        } catch(e) {
+            console.log(e);
+        }
+
+    }
--- src/socket.js
+++ src/socket.js
@@ -1,12 +1,11 @@
 import Server from 'socket.io';
 import cookieParser from 'socket.io-cookie';
-import { get } from 'lodash';
+import { get, reduce } from 'lodash';
 import jwt from 'jsonwebtoken';
 import config from 'config';
 import User from 'db/models/User';
 import Endpoint from 'db/models/Endpoint';
-import request from 'request';
-import { objectToQueryString } from 'lib/utility';
+import { pullImage, buildImage } from 'api/admin/image/image.ctrl';
 
 export default server => {
 
@@ -32,26 +31,9 @@ export default server => {
     });
 
     io.on('connection', socket => {
+        socket.on('pull', pullImage(socket));
+        socket.on('build', buildImage(socket));
 
-        const { endpoint: { url }} = socket.user;
+    });
 
-        socket.on('pull', (data,fn) => {
-
-            const queryString = objectToQueryString(JSON.parse(data)); 
-
-            let req = request({
-                method: 'POST',
-                uri: `${url}/images/create?${queryString}`
-            });
-
-            req.on( 'data', data => {
-                socket.emit('pullProgress', data.toString());
-            });
-
-            req.on('end', _ => {
-                socket.emit('pullEnd');
-            })
-        });
-
-    })
 }
