663a79cf4ae184f06e8c499127868571bbaf141c
Hosung Lim <holnet1026@gmail.com>
2018-09-30 23:51:54 +0900

    feat(container): implement container log api

--- src/api/admin/container/index.js
+++ src/api/admin/container/index.js
@@ -6,6 +6,7 @@ const router = new Router();
 router.get('/', containerCtrl.getContainerList);
 router.get('/:id', containerCtrl.getContainerInfo);
 router.get('/:id/raw', containerCtrl.getContainerInspectRaw);
+router.get('/:id/log', containerCtrl.getContainerLog);
 router.post('/prune', containerCtrl.pruneContainer);
 router.post('/create', containerCtrl.createContainer);
 router.post('/:id/:command', containerCtrl.commandToContainer);
