f729d73fb8c21181854ddf08644d857ededfe2ae
Hosung Lim <holnet1026@gmail.com>
2018-10-07 23:50:05 +0900

    feat(registry): implement registry image list api

--- src/lib/dockerApi/registry.js
+++ src/lib/dockerApi/registry.js
@@ -1,11 +1,54 @@
 import axios from 'axios';
-import { get } from 'lodash';
+import { get, assign, sumBy } from 'lodash';
+import { humanSize } from 'lib/utility';
 
-export const getSelectRegistryImage = (url) =>
-    axios.get(`${url}/v2/_catalog`)
-        .then(resp => {
-            const { data }  = resp;
-            return  {
-                image: get(data,'repositories','')
-            }
-        });
+export const selectAllRegistryImage = async ({
+	isAuth,
+	url,
+	username,
+	password
+}) => {
+	try {
+		let config = null;
+
+		if(isAuth){
+			config = {
+				auth: { username, password }
+			};
+		}	
+		
+		const { data } = await axios.get(`${url}/v2/_catalog`, config);
+
+		const repositories = get(data, 'repositories', []);	
+		
+
+		const promises = repositories.map(v => {
+			return new Promise(async(resolve,reject) => {
+				try {
+					const tagResponse = await axios.get(`${url}/v2/${v}/tags/list`, config);
+					const manifestResponse = await axios.get(`${url}/v2/${v}/manifests/latest`, assign({}, config, {
+						headers: {
+							accept: 'application/vnd.docker.distribution.manifest.v2+json'
+						}
+					}));
+
+					const tags = tagResponse.data.tags;
+					const manifest = manifestResponse.data;
+					
+					resolve({
+						name: v,
+						tags,
+						size: humanSize(sumBy(get(manifest, 'layers', []), 'size'))
+					})
+				} catch (e) {
+					/* handle error */
+					reject(e)
+				}
+			})
+		});
+
+		return Promise.all(promises);
+	} catch (e) {
+		throw e;
+	}
+}
