529783980a4ac156d8e18dbb2abca2ee33a7997f
Hosung Lim <holnet1026@gmail.com>
2018-10-24 22:22:35 +0900

    feat(ci): extract deploy script

---
+++ deploy/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+registry='localhost:5000'
+image='dockerfarm-backend'
+
+docker pull ${registry}/${image}
+docker rm -f ${image} || true
+docker system prune -af
+docker run -d -p 3000:3000  \
+    --restart always \
+    --name ${image} \
+    --link mongodb \
+    -v /home/dockerfarm/dockerfarm-backend/production.env:/usr/src/app/env/production.env \
+    ${registry}/${image}
